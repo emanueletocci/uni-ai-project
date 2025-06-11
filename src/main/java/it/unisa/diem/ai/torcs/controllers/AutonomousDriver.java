a5f4eb0ae7aeace38c7aaa9e815ba27e5a54171f
emanueletocci <tocciemanuele03@gmail.com>
2025-06-11 17:56:39 +0200

    Update AutonomousDriver.java

--- src/main/java/it/unisa/diem/ai/torcs/controllers/AutonomousDriver.java
+++ src/main/java/it/unisa/diem/ai/torcs/controllers/AutonomousDriver.java
@@ -8,37 +8,6 @@ import it.unisa.diem.ai.torcs.sensors.SensorModel;
 import it.unisa.diem.ai.torcs.utilities.FeatureNormalizer;
 
 public class AutonomousDriver extends BaseDriver {
-    final int[] gearUp = {5000, 6000, 6000, 6500, 7000, 0};
-    final int[] gearDown = {0, 2500, 3000, 3000, 3500, 3500};
-
-    final int stuckTime = 25;
-    final float stuckAngle = (float) 0.523598775; // PI/6
-
-    final float maxSpeedDist = 70;
-    final float maxSpeed = 150;
-    final float sin5 = (float) 0.08716;
-    final float cos5 = (float) 0.99619;
-
-    final float steerLock = (float) 0.785398;
-    final float steerSensitivityOffset = (float) 80.0;
-    final float wheelSensitivityCoeff = 1;
-
-    final float[] wheelRadius = {(float) 0.3179, (float) 0.3179, (float) 0.3276, (float) 0.3276};
-    final float absSlip = (float) 2.0;
-    final float absRange = (float) 3.0;
-    final float absMinSpeed = (float) 3.0;
-
-    final float clutchMax = (float) 0.5;
-    final float clutchDelta = (float) 0.05;
-    final float clutchRange = (float) 0.82;
-    final float clutchDeltaTime = (float) 0.02;
-    final float clutchDeltaRaced = 10;
-    final float clutchDec = (float) 0.01;
-    final float clutchMaxModifier = (float) 1.3;
-    final float clutchMaxTime = (float) 1.5;
-
-    private int stuck = 0;
-    private float clutch = 0;
     private int cicliRecupero = 0;
     private final NearestNeighbor knn;
     private final Action action;
